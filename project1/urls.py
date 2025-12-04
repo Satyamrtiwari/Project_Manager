e6fb80c67b54b5077f79c324126617803e48f8e4
Satyam <tiwarisatyamr10@gmail.com>
2025-12-04 21:28:34 +0530

    Add root URL response

--- project1/urls.py
+++ project1/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path , include
+from django.http import JsonResponse
 
 urlpatterns = [
+    path("", lambda request: JsonResponse({"status": "Backend is running"})),
     path('admin/', admin.site.urls),
     path('api/',include('app2.urls')),
 ]
